473263e0318f1ef4bfadb29e5873d57ed0a82493
Bojan Komazec <bojan.komazec@gmail.com>
2020-11-13 23:32:46 +0000

    Updated code for displaying image with OpenCV in standalone python script.

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 
 SHIFT + TAB = open Python documentation about function (when mouse hovers the function)
 SHIFT + ENTER = execute current cell (`[*]` next to cell means that it is currently executing)
+
+## Troubleshooting
+
+If for some reason cell remains indefinitely in executing state (`[*]`) we need to restart the kernel.
--- Section-03/13-mypuppy.py
+++ Section-03/13-mypuppy.py
@@ -1,4 +1,20 @@
 import cv2
 img = cv2.imread('../data/00-puppy.jpg')
+
+# cv2.imshow(window_name, matrix)
+# https://docs.opencv.org/master/d7/dfc/group__highgui.html#ga453d42fe4cb60e5723281a89973ee563
+# https://www.geeksforgeeks.org/python-opencv-cv2-imshow-method/
 cv2.imshow('Puppy', img)
-cv2.waitKey()
+
+while True:
+    # wait 1 ms
+    key = cv2.waitKey(1)
+
+    if key == -1:
+        continue
+
+    if key == 27:
+        print('ESC key has been pressed')
+        break
+
+cv2.destroyAllWindows()
